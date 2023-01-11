efece2a0df3a0daede4f10c35a6c18022011f3fc
laserzz <82984448+laserzz@users.noreply.github.com>
2023-01-11 20:30:46 +0000

    basic UserOptions interface

---
+++ src/interfaces/UserOptions.ts
@@ -0,0 +1,17 @@
+export interface UserOptions {
+    id: string
+    username: string
+    discriminator: string
+    avatar: string | null | undefined
+    bot?: boolean
+    system?: boolean
+    mfaEnabled?: boolean
+    banner?: string | null | undefined
+    accentColor?: number | null| undefined
+    locale?: string
+    verified?: boolean
+    email?: string | null | undefined
+    flags?: number
+    premiumType?: number
+    publicFlags?: number
+}
