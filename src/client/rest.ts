3bd67f858b443b87e37cab70a5c52ca1324192fd
laserzz <82984448+laserzz@users.noreply.github.com>
2023-01-10 17:11:21 -0800

    add http methods

--- src/client/rest.ts
+++ src/client/rest.ts
@@ -8,7 +8,7 @@ export class REST extends EventEmitter {
         this.token = token;
     }
 
-    async request(options: RequestOptions): Promise<void> {
+    async request(options: RequestOptions): Promise<any> {
         let method = options.method;
         this.emit('debug', `[HTTP] ${options.method} ${options.data ? JSON.stringify(options.data): ""}`);
         const resp = await fetch(`${Paths.API_PATH}${options.path}`, {
@@ -21,5 +21,27 @@ export class REST extends EventEmitter {
                 "authority": "https://discord.com"
             }
         });
+
+        return resp;
+    }
+
+    async get(options: RequestOptions) {
+        return await this.request({path: options.path, method: 'GET', data: options.data});
+    }
+
+    async post(options: RequestOptions) {
+        return await this.request({path: options.path, method: 'POST', data: options.data});
+    }
+
+    async patch(options: RequestOptions) {
+        return await this.request({path: options.path, method: 'PATCH', data: options.data});
+    }
+
+    async delete(options: RequestOptions) {
+        return await this.request({path: options.path, method: 'DELETE', data: options.data});
+    }
+
+    async put(options: RequestOptions) {
+        return await this.request({path: options.path, method: 'PUT', data: options.data});
     }
 }
