67700f9813dba22c3d52f08b10a0ee865655f43d
Jonathan55 <misnerjonathan58@gmail.com>
2023-01-11 17:30:31 -0700

    heartbeat

--- src/client/gateway.ts
+++ src/client/gateway.ts
@@ -28,6 +28,60 @@ export class GatewayClient extends EventEmitter {
         this.websocket.on("message", async data => {
             const payload = JSON.parse(data.toString());
             this.emit("debug", `[Gateway] Received ${JSON.stringify(payload)}`);
+            switch (payload.op) {
+                case (1):
+                    await this.heartbeatOnce({ "op": 1, "d": payload.d });
+                    break;
+
+                case (10):
+                    await this.heartbeat(payload.d.heartbeat_interval * Math.random(), { op: 1, d: payload.d })
+                    await this.identify(this.token);
+                    this.emit('ready');
+                    break;
+
+                case (11):
+                    this.emit('heartbeatACK');
+                    break;
+
+                case (0):
+                    await this.handleReceive(payload);
+                    break;
+            }
         })
+    }
+
+    async identify(token: string): Promise<void> {
+        const idPayload = {
+            op: OpCodes.IDENTIFY,
+            d: {
+                token: token,
+                intents: 32767,
+                properties: {
+                    os: "linux",
+                    browser: "my_library",
+                    device: "my_library"
+                }
+            }
+        }
+        this.websocket.send(JSON.stringify(idPayload));
+    }
+
+    async heartbeatOnce(payload: any) {
+        this.websocket.send(payload);
+        this.emit('heartbeat');
+    }
+
+    async heartbeat(interval: number, payload: object) {
+        setInterval(() => {
+            this.websocket.send(JSON.stringify(payload));
+            this.emit('heartbeat');
+        }, interval);
+    }
+
+    async handleReceive(payload:any) {
+        //const event = EventAliases[payload.t];
+        //console.log(payload.t);
+        //console.log(event);
+        //this.emit(event, (payload.d));
     }
 }
